6335ccb29d13f8d80066cb74d15e3c572934bec2
gauravdeshmukh224 <ggod71816@gmail.com>
2026-01-29 17:23:44 +0530

    Add files via upload

---
+++ sprint.js
@@ -0,0 +1,27 @@
+function openSection(type) {
+    if (type === "nakar") {
+        document.getElementById("content").innerHTML =
+        "<h2>नकार</h2><p>तक्रार माहिती...</p>";
+    }
+}
+
+function openSection(type) {
+    if (type === "magni") {
+        document.getElementById("content").innerHTML =
+        "<h2>नकार</h2><p>तक्रार माहिती...</p>";
+    }
+}
+
+function openSection(type) {
+    if (type === "nakar") {
+        document.getElementById("content").innerHTML =
+        "<h2>नकार</h2><p>तक्रार माहिती...</p>";
+    }
+}
+
+function openSection(type) {
+    if (type === "nakar") {
+        document.getElementById("content").innerHTML =
+        "<h2>नकार</h2><p>तक्रार माहिती...</p>";
+    }
+}
